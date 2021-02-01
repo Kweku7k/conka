72d639fce9867f4fd8b47ecbebc2c74e0e9c9c00
Nana Kweku <kweku@Nana-Kwekus-MacBook-Pro.local>
2021-02-01 10:38:03 +0000

    initial commit

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/ibuy')
+def ibuy():
+    return render_template('ibuy.html')
+
+@app.route('/<string:brand>')
+def products(brand):
+    print(brand)
+    return render_template('products.html')
+
+@app.route('/description')
+def description():
+    return render_template('description.html')
 if __name__ == '__main__':
     app.run(debug=True)
